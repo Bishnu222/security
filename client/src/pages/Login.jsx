15eb15f570952f0561a8c90b615c4a0f69c5fa27
Bishnu <bishnubudathoki717@gmail.com>
2026-01-28 15:06:53 +0545

    commit

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -14,7 +14,6 @@ const Login = () => {
     const [captchaSvg, setCaptchaSvg] = useState('');
     
 
-    // MFA State
     const [mfaRequired, setMfaRequired] = useState(false);
     const [mfaCode, setMfaCode] = useState('');
     const [tempToken, setTempToken] = useState('');
